fee5bd8f16ed45888739ec2f34196a2dc3c83af8
Sean Lynch <techniq35@gmail.com>
2025-02-10 08:15:50 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+	import { AreaChart } from 'layerchart';
+	import { data } from './data';
+</script>
+
+<main class="p-10">
+	<div class="h-[300px] p-4 border rounded">
+		<!-- Need to override `hsl(var(--color-primary))` since you define CSS color variables with colorspace functions in Tailwind 4 -->
+		<AreaChart
+			{data}
+			x="date"
+			y="value"
+			series={[{ key: 'value', color: 'var(--color-primary)' }]}
+		/>
+	</div>
+</main>
---
+++ src/routes/data.ts
@@ -0,0 +1,122 @@
+export const data = [
+	{
+		date: new Date('2025-01-11T23:00:00.000Z'),
+		value: 62
+	},
+	{
+		date: new Date('2025-01-12T23:00:00.000Z'),
+		value: 91
+	},
+	{
+		date: new Date('2025-01-13T23:00:00.000Z'),
+		value: 76
+	},
+	{
+		date: new Date('2025-01-14T23:00:00.000Z'),
+		value: 82
+	},
+	{
+		date: new Date('2025-01-15T23:00:00.000Z'),
+		value: 65
+	},
+	{
+		date: new Date('2025-01-16T23:00:00.000Z'),
+		value: 86
+	},
+	{
+		date: new Date('2025-01-17T23:00:00.000Z'),
+		value: 99
+	},
+	{
+		date: new Date('2025-01-18T23:00:00.000Z'),
+		value: 70
+	},
+	{
+		date: new Date('2025-01-19T23:00:00.000Z'),
+		value: 67
+	},
+	{
+		date: new Date('2025-01-20T23:00:00.000Z'),
+		value: 79
+	},
+	{
+		date: new Date('2025-01-21T23:00:00.000Z'),
+		value: 66
+	},
+	{
+		date: new Date('2025-01-22T23:00:00.000Z'),
+		value: 56
+	},
+	{
+		date: new Date('2025-01-23T23:00:00.000Z'),
+		value: 98
+	},
+	{
+		date: new Date('2025-01-24T23:00:00.000Z'),
+		value: 85
+	},
+	{
+		date: new Date('2025-01-25T23:00:00.000Z'),
+		value: 88
+	},
+	{
+		date: new Date('2025-01-26T23:00:00.000Z'),
+		value: 60
+	},
+	{
+		date: new Date('2025-01-27T23:00:00.000Z'),
+		value: 52
+	},
+	{
+		date: new Date('2025-01-28T23:00:00.000Z'),
+		value: 83
+	},
+	{
+		date: new Date('2025-01-29T23:00:00.000Z'),
+		value: 50
+	},
+	{
+		date: new Date('2025-01-30T23:00:00.000Z'),
+		value: 85
+	},
+	{
+		date: new Date('2025-01-31T23:00:00.000Z'),
+		value: 52
+	},
+	{
+		date: new Date('2025-02-01T23:00:00.000Z'),
+		value: 70
+	},
+	{
+		date: new Date('2025-02-02T23:00:00.000Z'),
+		value: 63
+	},
+	{
+		date: new Date('2025-02-03T23:00:00.000Z'),
+		value: 76
+	},
+	{
+		date: new Date('2025-02-04T23:00:00.000Z'),
+		value: 94
+	},
+	{
+		date: new Date('2025-02-05T23:00:00.000Z'),
+		value: 86
+	},
+	{
+		date: new Date('2025-02-06T23:00:00.000Z'),
+		value: 75
+	},
+	{
+		date: new Date('2025-02-07T23:00:00.000Z'),
+		value: 56
+	},
+	{
+		date: new Date('2025-02-08T23:00:00.000Z'),
+		value: 89
+	},
+	{
+		date: new Date('2025-02-09T23:00:00.000Z'),
+		value: 88
+	}
+];
